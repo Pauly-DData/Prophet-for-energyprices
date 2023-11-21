1b19ab156d780a82a72daa4a1e18478bc27da691
Paul Driessens <pdriessens07@gmail.com>
2023-11-21 16:36:36 +0100

    file update on fila_path

--- data_collection.py
+++ data_collection.py
@@ -3,7 +3,7 @@ from prophet import Prophet
 import matplotlib.pyplot as plt
 
 # The file path to your dataset
-file_path = 'C:/Spyder - Python/Clean/FINAL HOURLY MERGED dataset - Price Usagae Temp 20220901 - 20230901'
+file_path = "C:\Spyder - Python\Stichting Park Medisch Verbruik Dataset\Stichting Park Medisch Verbruik Dataset - Elektriciteit\Clean\FINAL HOURLY MERGED dataset - Price Usagae Temp 20220901 - 20230901"
 
 #read the file as a csv file in the new variable 'data'
 data = pd.read_csv(file_path)
