f3f96f64523ede75d37099d908c60a2170d21702
Paul Driessens <pdriessens07@gmail.com>
2023-11-22 15:02:47 +0100

    Second part of our model setup

--- Model setup.py
+++ Model setup.py
@@ -9,11 +9,30 @@ data_model = pd.read_csv(file_path)
 #just checking out if the dataset looks like we want it to be
 print(data_model.head())
 
-
 #for the prophet model we need to rename the columns to 'ds' (date column) and to 'y' that is the target column
 data_model.rename(columns={'Verbruik KwH': 'y', 'Datum': 'ds'}, inplace=True)
 
 print(data_model.head())
 
 #now that are df is corrected to something that fitst the model requirements we add a new variable that we will build the model on. we only need the ds en y variables. 
-prophet_data = data_model['ds', 'y']
+prophet_data = data_model[['ds', 'y']]
+
+#initialize the model - with this action we create a new Prophet model
+model = Prophet()
+
+# Fit the model with your dataframe - we then train our model based on our data. We trigger the model that is should start learning from the 'prophet_data'. 
+model.fit(prophet_data)
+
+#we create a new dataframe that will contain the predictions. With the 'periods=365' we look a 365 periods into the future and with 'freg=H' we tell the model that every period is one hour. 
+#we can adjust periods with the number of periods we want to forecast; so if we want to the model to predict one year we change periods=8760 or a prediction for the next day will be periods=24
+future = model.make_future_dataframe(periods=24, freq='H')
+
+#with forecast we instruct the model to do predictions based on the 'future' df. The model learns from the historical data to predict
+forecast = model.predict(future)
+
+# Display the forecast based for the next x hours. The ds in the df is equal to the time and time prediction. 
+forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(365)
+
+future = model.make_future_dataframe(periods=365, freq='H')
+forecast = model.predict(future)
+print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']])
